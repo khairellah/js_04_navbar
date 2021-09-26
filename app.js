702e4cdbea870efa12201cf29a45e28c7f6c2a23
khairellah <zakariakhairellah@gmail.com>
2021-09-26 10:55:53 +0200

    Add code js file

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", ()=>{
+    //get all classlist of element<
+    //console.log(links.classList);
+    // check if element contains a class colled show-links => boolean
+    //console.log(links.classList.contains("show-links"));
+
+    /* 
+    Old way to remove or add a classe into an element 
+    if(links.classList.contains("show-links")){
+        links.classList.remove("show-links")
+    }
+    else{
+        links.classList.add("show-links")}
+    */
+   links.classList.toggle("show-links");
+})
